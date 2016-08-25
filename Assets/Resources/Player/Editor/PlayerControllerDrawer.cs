adaa1bdd65ab6195fed006c1e7439289d159afe3
lazandrei19 <andrei_spion@yahoo.com>
2016-08-25 17:41:07 +0300

    Temporary fix for #2. Looks a bit off. Will tweak later

--- Assets/Resources/Player/Editor/PlayerControllerDrawer.cs
+++ Assets/Resources/Player/Editor/PlayerControllerDrawer.cs
@@ -8,6 +8,7 @@ public class PlayerControllerDrawer : Editor {
 	//All fields from PlayerController
 	SerializedProperty CameraReference;
 	SerializedProperty EyesReference;
+	SerializedProperty BackEyesReference;
 	SerializedProperty RotateAmount;
 	SerializedProperty CameraMovementRange;
 
@@ -18,6 +19,7 @@ public class PlayerControllerDrawer : Editor {
 		//Initialization of all property fields
 		CameraReference = serializedObject.FindProperty ("er_CameraTransform");
 		EyesReference = serializedObject.FindProperty ("er_EyesTransform");
+		BackEyesReference = serializedObject.FindProperty ("er_BackEyesTransform");
 		RotateAmount = serializedObject.FindProperty ("e_RotateAmount");
 		CameraMovementRange = serializedObject.FindProperty ("e_CameraMovementRange");
 		minVal = CameraMovementRange.vector2Value.x;
@@ -31,12 +33,17 @@ public class PlayerControllerDrawer : Editor {
 		//Field
 		EditorGUILayout.PropertyField (CameraReference, new GUIContent ("Camera Reference", "A reference to the Camera that follows the player"));
 		EditorGUILayout.PropertyField (EyesReference, new GUIContent ("Eyes Reference", "A reference to the eyes of the player. Used for raycasting and determining if movement is possible"));
+		EditorGUILayout.PropertyField (BackEyesReference, new GUIContent ("Back eyes Reference", "A reference to the point from which raycasts are shoot towards the camera to prevent clipping"));
 
 		EditorGUILayout.Space ();
 		RotateAmount.floatValue = EditorGUILayout.IntSlider (new GUIContent ("Rotate amount", "The amount of degrees to rotate when pressing the rotate button once"), Mathf.RoundToInt (RotateAmount.floatValue), 1, 360);
 
 		EditorGUILayout.Space ();
-		EditorGUILayout.LabelField (new GUIContent ("Camera movement range (" + minVal + ", " + maxVal + ")", "The values between the camera moves when trying to avoid clipping"));
+		EditorGUILayout.BeginHorizontal ();
+		EditorGUILayout.LabelField (new GUIContent ("Camera movement range", "The values between the camera moves when trying to avoid clipping"));
+		minVal = EditorGUILayout.FloatField ("", minVal, GUILayout.MaxWidth (80f));
+		maxVal = EditorGUILayout.FloatField ("", maxVal, GUILayout.MaxWidth (80f));
+		EditorGUILayout.EndHorizontal ();
 		EditorGUILayout.MinMaxSlider (new GUIContent (""), ref minVal, ref maxVal, -15f, 15f);
 
 		CameraMovementRange.vector2Value = new Vector2 (minVal, maxVal);
--- Assets/Resources/Player/Scripts/PlayerController.cs
+++ Assets/Resources/Player/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public Transform er_CameraTransform;								//The Transform of the camera
-	public Vector2 e_CameraMovementRange = new Vector2 (-6f, -0.5f);	//The range in which the camera is allowed to move on the Z-axis
+	public Vector2 e_CameraMovementRange = new Vector2 (-6f, 3f);		//The range in which the camera is allowed to move on the Z-axis
 
 	public float e_RotateAmount = 45f;									//The amount by which the player rotates when one of the rotation keys is pressed
 
 	public Transform er_EyesTransform;									//The Transform of the eyes
+	public Transform er_BackEyesTransform;								//The point from where the backwards raycast shoots towards the camera
 
 	RaycastHit cameraHit;
 
@@ -16,6 +17,8 @@ public class PlayerController : MonoBehaviour {
 	bool shouldMove = true;
 	bool isOnDiag = false;
 	bool cameraClippingDirty = false;
+
+	float maxCameraRaycast;
 
 	int RotateDirection {
 		get {
@@ -56,8 +59,14 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Awake () {
-		//Hide the cursor and lock it in the middle
+		// Hide the cursor and lock it in the middle
 		Cursor.lockState = CursorLockMode.Locked;
+		// Calculates the maximum practical distance for the raycast
+		maxCameraRaycast = Mathf.Max (Mathf.Abs (e_CameraMovementRange.x), Mathf.Abs (e_CameraMovementRange.y));
+	}
+
+	void Start () {
+		
 	}
 
 	void MovePlayer () {
@@ -86,21 +95,39 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void PreventCameraClipping () {
+		
 		if (!cameraClippingDirty) {
 			return;
 		}
 
 		er_CameraTransform.localPosition = new Vector3 (0, 1, e_CameraMovementRange.x);
+		er_CameraTransform.localEulerAngles = new Vector3 (30, 0, 0);
 
-		if (!Physics.Raycast (er_CameraTransform.position, er_CameraTransform.TransformDirection (Vector3.forward), out cameraHit, 5f)) {
+		if (!Physics.Raycast (er_BackEyesTransform.position, er_CameraTransform.TransformDirection (Vector3.back), out cameraHit, maxCameraRaycast)) {
+			cameraClippingDirty = false;
 			return;
 		}
+
+		Debug.Log ("hiyt");
 
 		if (!cameraHit.collider.HasTag ("CameraCollider")) {
+			cameraClippingDirty = false;
 			return;
 		}
 
-		er_CameraTransform.localPosition = new Vector3 (0, 1, Mathf.Clamp(e_CameraMovementRange.x + (cameraHit.point - er_CameraTransform.position).magnitude, e_CameraMovementRange.x, e_CameraMovementRange.y));
+		Debug.Log ((cameraHit.point - er_CameraTransform.position).magnitude);
+
+		er_CameraTransform.localPosition = new Vector3 (0, 1, e_CameraMovementRange.x + (cameraHit.point - er_CameraTransform.position).magnitude);
+
+		if (er_CameraTransform.localPosition.z > -2f && er_CameraTransform.localPosition.z < 2.5f) {
+			er_CameraTransform.localPosition = new Vector3 (0f, 1f, 3f);
+		}
+
+		if (er_CameraTransform.localPosition.z > -2f) {
+			er_CameraTransform.localEulerAngles = new Vector3 (30, 180, 0);
+		}
+
+		cameraClippingDirty = false;
 	}
 
 	void RotatePlayer () {
@@ -127,7 +154,8 @@ public class PlayerController : MonoBehaviour {
 	//Activates on "Reset component"
 	void Reset () {
 		er_CameraTransform = Camera.main.transform;
-		e_CameraMovementRange = new Vector2 (-6f, -0.5f);
+		e_CameraMovementRange = new Vector2 (-6f, 3f);
+		e_RotateAmount = 45f;
 	}
 
 	void FixedUpdate () {
